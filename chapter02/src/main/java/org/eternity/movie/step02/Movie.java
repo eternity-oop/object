7c1570e22e6bb9fdf1a808d1725bf4fce97c8b11
jongfeel <DeveloperFEEL@gmail.com>
2021-01-21 01:32:26 +0900

    Add missing null check discountPolicy

--- chapter02/src/main/java/org/eternity/movie/step02/Movie.java
+++ chapter02/src/main/java/org/eternity/movie/step02/Movie.java
@@ -22,6 +22,10 @@ public class Movie {
     }
 
     public Money calculateMovieFee(Screening screening) {
+        if (discountPolicy == null) {
+            return fee;
+        }
+        
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
