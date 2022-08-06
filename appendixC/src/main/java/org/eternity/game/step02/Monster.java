5e2e6314f204dd0970a38da0cec9d8d10d753520
Haseop Lee <haservi@naver.com>
2022-08-06 09:25:07 +0900

    [Bug Fix] Breed 클래스를 할당 받도록 수정
    step01의 개선코드에서 health가 아닌 breed 변수에 Breed 클래스를 할당받도록 수정하였습니다.

--- appendixC/src/main/java/org/eternity/game/step02/Monster.java
+++ appendixC/src/main/java/org/eternity/game/step02/Monster.java
@@ -1,11 +1,10 @@
 package org.eternity.game.step02;
 
 public class Monster {
-	private int health;
 	private Breed breed;
 	
 	public Monster(Breed breed) {
-		this.health = breed.getHealth();
+		this.breed = breed;
 	}
 	
 	public String getAttack() {
